3b7d28224f2d501f8a67dbaa007ee4ad8a71aad9
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-09-23 22:56:20 -0500

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -38,6 +38,8 @@ with col1:
         st.button("A.4 DHS DATA", key="A4_DHS_data", on_click=navigate_to, args=("A.4 DHS DATA",))
         st.button("A.5 CLIMATE DATA", key="A5_Climate_data", on_click=navigate_to, args=("A.5 CLIMATE DATA",))
         st.button("A.6 LMIS DATA", key="A6_LMIS_data", on_click=navigate_to, args=("A.6 LMIS DATA",))
+        st.button("A.6 MODELED DATA", key="A6_LMIS_data", on_click=navigate_to, args=("A.6 LMIS DATA",))
+        st.button("A.6 POPULATION DATA", key="A6_LMIS_data", on_click=navigate_to, args=("A.6 LMIS DATA",))
 
     
 
