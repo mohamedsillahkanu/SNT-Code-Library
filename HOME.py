2fdec681a1a00c67a239ad38873ab75355ef927c
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-09-26 16:57:43 -0500

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -17,7 +17,7 @@ with st.sidebar.expander("Overview", expanded=True):
 
 with st.sidebar.expander("A. DATA ASSEMBLY AND MANAGEMENT", expanded=False):
     st.write("""
-    - [A.1 Shapefiles]
+    - **A.1 Shapefiles**
      -[A.1.1 Import shapefiles](#import-shapefiles)
     - [A.2 Health Facilities](#health-facilities)
     - [A.3 Routine Case Data from DHIS2](#routine-case-data-from-dhis2)
