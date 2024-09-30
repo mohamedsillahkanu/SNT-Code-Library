158fe65198622c492e5c04545cdc1967c596d6ef
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-09-30 12:58:52 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,18 @@
-// JavaScript for collapsible menus
-const collapsibles = document.getElementsByClassName("collapsible");
-
-for (let i = 0; i < collapsibles.length; i++) {
-  collapsibles[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    const content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
 // Function to load content dynamically from other HTML files
-function loadContent(page) {
+function loadContent(page, element) {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
     document.getElementById("page-content").innerHTML = this.responseText;
   }
   xhttp.open("GET", page, true);
   xhttp.send();
+
+  // Remove 'active' class from all menu links
+  const menuLinks = document.getElementsByClassName("menu-link");
+  for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].classList.remove("active");
+  }
+
+  // Add 'active' class to the clicked link
+  element.classList.add("active");
 }
