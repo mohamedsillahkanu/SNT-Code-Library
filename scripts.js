aecc1460b4955cfe364acb43466d8cac615eafb8
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-09-30 12:49:23 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
+// JavaScript for collapsible menus
+const collapsibles = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < collapsibles.length; i++) {
+  collapsibles[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// Function to load content dynamically from other HTML files
+function loadContent(page) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById("page-content").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", page, true);
+  xhttp.send();
+}
 
